e938d7aeb9151c2a0d4488bc75ab5095de8d3fc5
xdlysk <xdlysk@live.cn>
2018-01-30 19:08:00 +0800

    update eureka

--- restdemo/src/main/java/com/xdlysk/restdemo/RestdemoApplication.java
+++ restdemo/src/main/java/com/xdlysk/restdemo/RestdemoApplication.java
@@ -2,8 +2,10 @@ package com.xdlysk.restdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class RestdemoApplication {
 
 	public static void main(String[] args) {
