4bf746b5ea80b63ed06a89deb116ce39e0c3535e
xdlysk <xdlysk@live.cn>
2018-02-11 17:32:42 +0800

    spring cloud config 2

---
+++ config-client-demo/src/main/java/com/xdlysk/configclientdemo/ConfigClientController.java
@@ -0,0 +1,21 @@
+package com.xdlysk.configclientdemo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.config.environment.Environment;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RefreshScope
+public class ConfigClientController {
+
+    @Value("${profile}")
+    private String profile;
+
+    @GetMapping("/profile")
+    public String hello(){
+        return profile;
+    }
+}
